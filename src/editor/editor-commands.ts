e5d8a69182bf86fa57c3e5f92cccce33f80d2e2b
Martin Emmert <martin@martinemmert.de>
2022-06-28 01:01:04 +0200

    ✨ add joinNoteBackward command when pressing Backspace
    wip #4

--- src/editor/editor-commands.ts
+++ src/editor/editor-commands.ts
@@ -1,4 +1,9 @@
-import { AllSelection, Command, TextSelection } from "prosemirror-state";
+import {
+  AllSelection,
+  Command,
+  Selection,
+  TextSelection,
+} from "prosemirror-state";
 import { Fragment, NodeRange, Slice } from "prosemirror-model";
 import { isTargetNodeOfType, mapChildren } from "./editor-utils";
 import {
@@ -134,7 +139,93 @@ export const liftNote: Command = (state, dispatch) => {
 };
 
 export const joinNoteBackward: Command = (state, dispatch) => {
-  return false;
+  const { $cursor } = state.selection as TextSelection;
+
+  // cancel if no cursor is available or if it is not at the beginning of the note
+  if (!$cursor || $cursor.parentOffset > 0) return false;
+
+  const noteTextNode = state.schema.nodes.note_text;
+  const noteChildrenNode = state.schema.nodes.note_children;
+
+  // allow usage only in a note_text node
+  if (!isTargetNodeOfType($cursor.parent, noteTextNode)) return false;
+
+  // get the start position of the current note
+  const $noteStart = state.doc.resolve($cursor.before($cursor.depth - 1));
+
+  // get the end position of the current note
+  const $noteEnd = state.doc.resolve($cursor.after($cursor.depth - 1));
+
+  // check if the current note is a nested note
+  const isNestedNote = isTargetNodeOfType($noteStart.parent, noteChildrenNode);
+
+  // check if the current note is the first note
+  const isFirstNote = $noteStart.nodeBefore === null;
+
+  // check if sibling notes are present
+  const hasSiblings = $noteStart.parent.childCount > 1;
+
+  // check if note is empty
+  const isEmptyNote =
+    $noteStart.nodeAfter?.textContent.trim() === "" &&
+    $noteStart.nodeAfter.childCount <= 1;
+
+  // check if previous note is empty
+  // note: does not guaranty that a previous note exists
+  const isPreviousNoteEmpty =
+    $noteStart.nodeBefore?.textContent?.trim() === "" &&
+    $noteStart.nodeBefore.childCount <= 1;
+
+  // check if previous note is a flat note (no children)
+  // note: does not guaranty that a previous note exists
+  const isPreviousNoteFlat =
+    !isPreviousNoteEmpty &&
+    isTargetNodeOfType($noteStart.nodeBefore?.lastChild, noteTextNode);
+
+  const tr = state.tr;
+
+  switch (true) {
+    // delete note if empty, not nested, and not firstNote
+    // delete note if empty, nested, and has siblings
+    case isEmptyNote && !isNestedNote && !isFirstNote:
+    case isEmptyNote && isNestedNote && hasSiblings:
+      tr.delete($noteStart.pos, $noteEnd.pos);
+      break;
+    // delete note and its outer node if empty, nested, and the only child
+    case isEmptyNote && isNestedNote && !hasSiblings:
+      tr.delete($noteStart.pos - 1, $noteEnd.pos + 1);
+      break;
+    // delete previous note if current is not empty, not the first one and previous note is empty
+    case !isEmptyNote && !isFirstNote && isPreviousNoteEmpty:
+      tr.delete(
+        $noteStart.pos - ($noteStart.nodeBefore?.nodeSize ?? 0),
+        $noteStart.pos
+      );
+      break;
+    // join with previous note if current is not empty, not the first and previous note is flat
+    case !isEmptyNote && !isFirstNote && isPreviousNoteFlat:
+      tr.join($noteStart.pos, 2);
+      break;
+    // cancel command if no case fits the current state
+    default:
+      return false;
+  }
+
+  if (dispatch) {
+    const newTextSelection = TextSelection.findFrom(
+      tr.doc.resolve(tr.mapping.map($cursor.pos)),
+      -1,
+      true
+    );
+
+    if (newTextSelection) tr.setSelection(newTextSelection);
+
+    tr.scrollIntoView();
+
+    dispatch(tr);
+  }
+
+  return true;
 };
 
 export const joinNoteForward: Command = (state, dispatch) => {
