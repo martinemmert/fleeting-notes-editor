6004ecc0e968b0ccdc00c9eadc16de2210370057
Martin Emmert <martin@martinemmert.de>
2022-06-28 01:01:04 +0200

    ✨ add joinNoteBackward command when pressing Backspace
    wip #4

--- src/editor/editor-utils.ts
+++ src/editor/editor-utils.ts
@@ -1,7 +1,9 @@
 import { Node, NodeType } from "prosemirror-model";
 
-export const isTargetNodeOfType = (node: Node, type: NodeType) =>
-  node.type === type;
+export const isTargetNodeOfType = (
+  node: Node | null | undefined,
+  type: NodeType
+) => node !== null && typeof node !== "undefined" && node.type === type;
 
 export const nodeHasAttribute = (node: Node, attrName: string) =>
   Boolean(node.attrs && node.attrs[attrName]);
