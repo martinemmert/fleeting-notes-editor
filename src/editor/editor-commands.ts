9fecc4aa14d29f3715b1d62887601cacda94129a
Martin Emmert <martin@martinemmert.de>
2022-06-28 13:49:48 +0200

    ♻️ try to join first before deleting with joinNoteBackward
    wip #4

--- src/editor/editor-commands.ts
+++ src/editor/editor-commands.ts
@@ -146,62 +146,58 @@ export const joinNoteBackward: Command = (state, dispatch) => {
   if (!isTargetNodeOfType($cursor.parent, noteTextNode)) return false;
 
   // get the start position of the current note
-  const $noteStart = state.doc.resolve($cursor.before($cursor.depth - 1));
+  const $start = state.doc.resolve($cursor.before($cursor.depth - 1));
 
   // get the end position of the current note
-  const $noteEnd = state.doc.resolve($cursor.after($cursor.depth - 1));
+  const $end = state.doc.resolve($cursor.after($cursor.depth - 1));
 
   // check if the current note is a nested note
-  const isNestedNote = isTargetNodeOfType($noteStart.parent, noteChildrenNode);
+  const isNestedNote = isTargetNodeOfType($start.parent, noteChildrenNode);
 
   // check if the current note is the first note
-  const isFirstNote = $noteStart.nodeBefore === null;
+  const isFirstNote = $start.nodeBefore === null;
 
   // check if sibling notes are present
-  const hasSiblings = $noteStart.parent.childCount > 1;
+  const hasSiblings = $start.parent.childCount > 1;
 
   // check if note is empty
   const isEmptyNote =
-    $noteStart.nodeAfter?.textContent.trim() === "" &&
-    $noteStart.nodeAfter.childCount <= 1;
+    $start.nodeAfter?.textContent.trim() === "" &&
+    $start.nodeAfter.childCount <= 1;
+
+  // check if previous note is a flat note (no children)
+  // note: does not guaranty that a previous note exists
+  const isPreviousNoteFlat = isTargetNodeOfType(
+    $start.nodeBefore?.lastChild,
+    noteTextNode
+  );
 
   // check if previous note is empty
   // note: does not guaranty that a previous note exists
   const isPreviousNoteEmpty =
-    $noteStart.nodeBefore?.textContent?.trim() === "" &&
-    $noteStart.nodeBefore.childCount <= 1;
-
-  // check if previous note is a flat note (no children)
-  // note: does not guaranty that a previous note exists
-  const isPreviousNoteFlat =
-    !isPreviousNoteEmpty &&
-    isTargetNodeOfType($noteStart.nodeBefore?.lastChild, noteTextNode);
+    $start.nodeBefore?.textContent?.trim() === "" &&
+    $start.nodeBefore.childCount <= 1;
 
   const tr = state.tr;
 
   switch (true) {
-    // delete note if empty, not nested, and not firstNote
-    // delete note if empty, nested, and has siblings
-    case isEmptyNote && !isNestedNote && !isFirstNote:
-    case isEmptyNote && isNestedNote && hasSiblings:
-      tr.delete($noteStart.pos, $noteEnd.pos);
+    // join with previous note if it is flat and not empty
+    case !isFirstNote && isPreviousNoteFlat && !isPreviousNoteEmpty:
+      tr.join($start.pos, 2);
       break;
-    // delete note and its outer node if empty, nested, and the only child
-    case isEmptyNote && isNestedNote && !hasSiblings:
-      tr.delete($noteStart.pos - 1, $noteEnd.pos + 1);
+    // delete previous note if it is flat and empty
+    case !isFirstNote && isPreviousNoteFlat && isPreviousNoteEmpty:
+      tr.delete($start.pos - $start.nodeBefore!.nodeSize, $start.pos);
       break;
-    // delete previous note if current is not empty, not the first one and previous note is empty
-    case !isEmptyNote && !isFirstNote && isPreviousNoteEmpty:
-      tr.delete(
-        $noteStart.pos - ($noteStart.nodeBefore?.nodeSize ?? 0),
-        $noteStart.pos
-      );
+    // delete current note if it is empty and has siblings
+    case isEmptyNote && hasSiblings:
+      tr.delete($start.pos, $end.pos);
       break;
-    // join with previous note if current is not empty, not the first and previous note is flat
-    case !isEmptyNote && !isFirstNote && isPreviousNoteFlat:
-      tr.join($noteStart.pos, 2);
+    // delete current note and its wrapping node if it is empty, has no siblings and is nested
+    case isEmptyNote && !hasSiblings && isNestedNote:
+      tr.delete($start.pos - 1, $end.pos + 1);
       break;
-    // cancel command if no case fits the current state
+    // cancel if no case
     default:
       return false;
   }
